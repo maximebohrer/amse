2e4416b59e965a6c919f6f002c5c592a3880a5bc
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-02-02 12:36:01 +0100

    ok

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ex3.dart';
 import 'ex1.dart';
 import 'ex2.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: const Exercice2(),
+      home: Exercice3(),
     );
   }
 }
