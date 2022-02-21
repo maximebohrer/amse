c24e5114bcbe0cdd97ffc54f908da1b8c026e776
maximebohrer <maxime21.bohrer@gmail.com>
2022-02-21 15:45:18 +0100

    fin ex 5a, creation ex 5b

---
+++ tp2/lib/ex5a.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class Exercice5a extends StatelessWidget {
+  const Exercice5a({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    math.Random random = math.Random();
+    return Scaffold(
+      appBar: AppBar(title: const Text("GridView Example (random colors)")),
+      body: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          Container(
+            child: const Center(
+                child: Text("1",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("2",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("3",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("4",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("5",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("6",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("7",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("8",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+          Container(
+            child: const Center(
+                child: Text("9",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 7,
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            color: HSVColor.fromAHSV(1, random.nextInt(360).toDouble(), 0.75, 1).toColor(),
+          ),
+        ],
+      ),
+    );
+  }
+}
