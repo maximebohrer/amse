6a7990c785b582da4968e8c8f148ef1bb95dbda5
maximebohrer <maxime21.bohrer@gmail.com>
2022-02-22 14:32:17 +0100

    ex 6b fonctionne

--- tp2/lib/ex6b.dart
+++ tp2/lib/ex6b.dart
@@ -12,6 +12,7 @@ class Exercice6b extends StatefulWidget {
 
 class _Exercice6bState extends State<Exercice6b> {
   int n = 3;
+  late int blankIndex;
   bool mustCreateTileList = true;
   late List<ColorTile> tileList;
 
@@ -37,7 +38,13 @@ class _Exercice6bState extends State<Exercice6b> {
               crossAxisSpacing: 2.5,
               padding: const EdgeInsets.all(20.0),
               shrinkWrap: true,
-              children: tileList.map((e) => e.getWidget()).toList(),
+              children: tileList
+                  .map((e) => e.getWidget(onTap: () {
+                        setState(() {
+                          moveToBlank(tileList.indexOf(e), Direction.all);
+                        });
+                      }))
+                  .toList(),
             ),
 
             //Slider n
@@ -70,6 +77,57 @@ class _Exercice6bState extends State<Exercice6b> {
     );
   }
 
+  bool moveToBlank(int i, Direction d) {
+    bool directionFound = false;
+    if ((d == Direction.up || d == Direction.all) && !directionFound) {
+      //i-n est l'indice de la case en haut de i
+      if ((i - n) >= 0 && !tileList[i - n].isEmpty) {
+        moveToBlank(i - n, Direction.up); //La fonction est récursive pour permettre de bouger plusieurs cases d'un coup
+      }
+      if ((i - n) >= 0 && tileList[i - n].isEmpty) {
+        switchElements(tileList, i, i - n);
+        return true;
+      }
+    }
+    if ((d == Direction.down || d == Direction.all) && !directionFound) {
+      //i+n est l'indice de la case en bas de i
+      if ((i + n) < n * n && !tileList[i + n].isEmpty) {
+        moveToBlank(i + n, Direction.down);
+      }
+      if ((i + n) < n * n && tileList[i + n].isEmpty) {
+        switchElements(tileList, i, i + n);
+        return true;
+      }
+    }
+    if ((d == Direction.left || d == Direction.all) && !directionFound) {
+      //i-1 est l'indice de la case à gauche de i si i%n > 0
+      if ((i % n) > 0 && !tileList[i - 1].isEmpty) {
+        moveToBlank(i - 1, Direction.left);
+      }
+      if ((i % n) > 0 && tileList[i - 1].isEmpty) {
+        switchElements(tileList, i, i - 1);
+        return true;
+      }
+    }
+    if ((d == Direction.right || d == Direction.all) && !directionFound) {
+      //i+1 est l'indice de la case à gauche de i si (i+1)%n > 0
+      if (((i + 1) % n) > 0 && !tileList[i + 1].isEmpty) {
+        moveToBlank(i + 1, Direction.right);
+      }
+      if (((i + 1) % n) > 0 && tileList[i + 1].isEmpty) {
+        switchElements(tileList, i, i + 1);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void switchElements(List list, int index1, int index2) {
+    var temp = list[index2];
+    list[index2] = list[index1];
+    list[index1] = temp;
+  }
+
   List<ColorTile> createTileList() {
     //Construction de la liste des tiles
     List<ColorTile> tileList = [];
@@ -80,7 +138,10 @@ class _Exercice6bState extends State<Exercice6b> {
         );
       }
     }
-    tileList[randomInt(n * n)].isEmpty = true;
+    blankIndex = randomInt(n * n);
+    tileList[blankIndex].isEmpty = true;
     return tileList;
   }
 }
+
+enum Direction { all, up, down, left, right }
